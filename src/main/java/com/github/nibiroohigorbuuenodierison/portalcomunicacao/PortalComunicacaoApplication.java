d45e609013c2b8d8679b554ff0950b1980bf9dad
Guilherme Carvalho <55836497+nibiroo@users.noreply.github.com>
2023-03-30 00:05:00 -0300

    Começouuuu o TCC caraioooo

---
+++ src/main/java/com/github/nibiroohigorbuuenodierison/portalcomunicacao/PortalComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.github.nibiroohigorbuuenodierison.portalcomunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalComunicacaoApplication.class, args);
+	}
+
+}
